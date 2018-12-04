ecfdf66d46864d903e88ee0637da0deb12215797
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2018-12-04 11:47:05 +0100

    Improved readability and replaced json file construct

--- bot.js
+++ bot.js
@@ -1,42 +1,54 @@
-const express = require('express')
-const path = require('path')
+require('dotenv').load();
+
+/* Module loading */
+const express = require('express');
+const Discord = require('discord.io');
+const logger = require('winston');
+const path = require('path');
 const PORT = process.env.PORT || 5000
 const IDENTIFIER = "*";
 
+/* Import command handlers */
+var commands = require("./commands.js");
+
+
+/* Express Server for pinging the bot on Heroku */
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.status(200).send("Pinged bot!"))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var commands = require("./commands.js");
-// Configure logger settings
+/* Initialize Discord bot */
+  var bot = new Discord.Client({
+     token: process.env.DISCORD_TOKEN,
+     autorun: true
+  });
+
+/* Configure logger for Heroku */
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
+
+
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == IDENTIFIER) {
+
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+
         args = args.splice(1);
         try {
             switch(cmd) {
